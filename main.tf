f220f38eef0482baed70a60c8e61a1b4572bdfa8
Hemanth-2310 <Saihemanthkumarpilli23@gmail.com>
2025-11-15 23:25:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ provider "aws" {
 region = "us-east-1"
 }
 
-resource "aws_vpc" "one" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "dev-vpc"
-  }
-}
-
-resource "aws_subnet" "two" {
-  vpc_id     = aws_vpc.one.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "dev-subent"
-  }
-}
-
 resource "aws_instance" "three" {
   count         = 4
   subnet_id     = aws_subnet.two.id
