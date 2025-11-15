2902ba0099512db6ecb26bf2e5ca3d026e64d564
Hemanth-2310 <Saihemanthkumarpilli23@gmail.com>
2025-11-15 23:31:30 +0530

    Add VPC and subnet resources to main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,25 @@ provider "aws" {
 region = "us-east-1"
 }
 
+resource "aws_vpc" "one" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "two" {
+  vpc_id     = aws_vpc.one.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev-subent"
+  }
+}
+
 resource "aws_instance" "three" {
   count         = 4
+  subnet_id     = aws_subnet.two.id
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t3.micro"
   tags = {
