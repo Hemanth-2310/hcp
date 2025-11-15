f75a9400ac39972c9690c4afa346c7d8b4444d7c
Hemanth-2310 <Saihemanthkumarpilli23@gmail.com>
2025-11-15 23:02:02 +0530

    Modify AWS instance configuration in main.tf
    Updated AMI ID and key name for AWS instance resource.

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-03695d52f0d883f65"
+  ami                    = "ami-0c02fb55956c7d316"  
   instance_type          = "t3.micro"
-  key_name               = "saikeypair"
+  key_name               = "sai key pair"
   vpc_security_group_ids = ["sg-0b41495a016b8f79d"]
+
   tags = {
     Name = var.instance_names[count.index]
   }
-}
-
-variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
